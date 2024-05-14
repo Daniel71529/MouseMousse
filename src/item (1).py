0aaf7c79b5c9ee3996bbfa1bdcdc3b6c78ab5b78
Daniel71529 <142557105+Daniel71529@users.noreply.github.com>
2024-05-14 09:36:18 -0600

    Add files via upload

---
+++ src/item (1).py
@@ -0,0 +1,12 @@
+class Item:
+  def __init__(self, type, amount_health):
+    self.type = type
+    self.amount_health = amount_health
+
+  def drink(self, mouse): 
+    if self.type == 'health':
+      mouse.health += self.amount_health
+      print('You drank the potion and gained', self.amount_health , 'health!')
+      print('Your health is now', mouse.health)
+ 
+    
